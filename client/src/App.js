71e27ecb9ebc57d078c5bc96a7da9059ab851952
koudjo-simon <pro.simonkoudjo.9204@gmail.com>
2023-04-20 16:30:58 +0200

    Form data recuperation 1

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,24 @@ import Formulaire1 from "./Components/Pages/inscription/Formulaire1";
 import Formulaire2 from "./Components/Pages/inscription/Formulaire2";
 import Formulaire3 from "./Components/Pages/inscription/Formulaire3";
 import LayoutGeneral from "./Components/Layouts/LayoutGeneral";
+import { useState } from "react";
 
 const App = () => {
+
+  const [nom, setNom] = useState("Martin");
+  const [prenom, setPrenom] = useState("");
+  const [pseudo, setPseudo] = useState("");
+  const [email, setEmail] = useState("");
+  const [mdp, setMdp] = useState("");
+  const [confMdp, setConfMdp] = useState("");
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LayoutGeneral />}>
             <Route index element={<Acceuil />} />
-            <Route path="inscription1" element={<Formulaire1/>} />
+            <Route path="inscription1" element={<Formulaire1 nom={nom} prenom={prenom} pseudo={pseudo} email={email} mdp={mdp} confMdp={confMdp} setNom={setNom} setPrenom={setPrenom} setPseudo={setPseudo} setEmail={setEmail} setMdp={setMdp} setConfMdp={setConfMdp} />} />
             <Route path="inscription2" element={<Formulaire2/>} />
             <Route path="inscription3" element={<Formulaire3/>} />
           </Route>
--- client/src/Components/Pages/inscription/Formulaire1.jsx
+++ client/src/Components/Pages/inscription/Formulaire1.jsx
@@ -1,20 +1,29 @@
+import { json } from 'react-router-dom';
 import classe from './formulaire1.module.css'
 
-const Formulaire1 = (props) => {
+const Formulaire1 = ({nom, prenom, pseudo, email, mdp, confMdp, setNom, setPrenom, setPseudo, setEmail, setMdp, setConfMdp}) => {
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        let data = {nom, prenom, pseudo, email, mdp, confMdp};
+        console.log(`${JSON.stringify(data)}`);
+    }
 
     return (
         <>
             <div className={classe.form}>
                 <div className={classe.noitr}>
                     <div className={classe.form_container}>
-                        <form>
+                        <form onSubmit={(e)=>{handleSubmit(e)}}>
                             <table>
                                 <tbody>
                                 <tr className={classe.form_tr_mb}>
                                     <td className={classe.label_td}><label htmlFor="nom"><strong>Nom: </strong></label></td>
                                     <td>
                                         <input type="text"
-                                            className={classe.input_auth} id="nom" name="nom"/>
+                                            className={classe.input_auth}
+                                            onChange={(e)=>{setNom(e.target.value)}}
+                                             id="nom" name="nom" required/>
                                         </td>
                                 </tr>
                             
@@ -22,40 +31,50 @@ const Formulaire1 = (props) => {
                                     <td className={classe.label_td}><label htmlFor="prenom"><strong> Prénom: </strong> </label></td>
                                     <td>
                                         <input type="text"
-                                            className={classe.input_auth} id="prenom" name="prenom"/>
+                                            className={classe.input_auth}
+                                            onChange={(e)=>{setPrenom(e.target.value)}}
+                                            id="prenom" name="prenom" required/>
                                     </td>
                                 </tr>
                                 <tr className={classe.form_tr_mb}>
                                     <td className={classe.label_td}><label htmlFor="pseudo"><strong>Pseudo: </strong>  </label></td>
                                     <td>
                                         <input type="text"
-                                            className={classe.input_auth} id="pseudo" name="pseudo"/>
+                                            className={classe.input_auth}
+                                            onChange={(e)=>{setPseudo(e.target.value)}}
+                                            id="pseudo" name="pseudo" required/>
                                     </td>
                                 </tr>
                                 <tr className={classe.form_tr_mb}>
                                     <td className={classe.label_td}><label htmlFor="email"><strong>Email:</strong>  </label></td>
                                     <td>
                                         <input type="email"
-                                            className={classe.input_auth} id="email" name="email"/>
+                                            className={classe.input_auth}
+                                            onChange={(e)=>{setEmail(e.target.value)}}
+                                            id="email" name="email" required/>
                                     </td>
                                 </tr>
                                 <tr className={classe.form_tr_mb}>
                                     <td className={classe.label_td}><label htmlFor="mdp"><strong>Mot de passe: </strong>  </label></td>
                                     <td>
                                         <input type="password"
-                                            className={classe.input_auth} id="mdp" name="mdp"/>
+                                            className={classe.input_auth}
+                                            onChange={(e)=>{setMdp(e.target.value)}}
+                                            id="mdp" name="mdp" required/>
                                     </td>
                                 </tr>
                                 <tr className={classe.form_tr_mb}>
                                     <td className={classe.label_td}><label htmlFor="confirm_mdp"><strong>Confirmez le mot de passe: </strong>  </label></td>
                                     <td>
                                         <input type="password"
-                                            className={classe.input_auth} id="confirm_mdp" name="confirm_mdp"/>
+                                            className={classe.input_auth} 
+                                            onChange={(e)=>{setConfMdp(e.target.value)}}
+                                            id="confirm_mdp" name="confirm_mdp" required/>
                                     </td>
                                 </tr>
                                 </tbody>
                             </table> 
-                            <button className={classe.btn_inscription} onClick={props.onNext}>Suivant</button>
+                            <button className={classe.btn_inscription}>Suivant</button>
                         </form>
                     </div>
                 </div>
